e98805d6deb2d5b5d876ad67da41a63be01581bf
VladislavIlin7 <svladilin@gmail.com>
2025-09-26 14:33:43 +0300

    add: calculator

--- src/main.py
+++ src/main.py
@@ -115,6 +115,36 @@ def tokens_to_RPN(tokens: array[Token]) -> array[Token]:
 
     return output
 
+def calculator(tokens: array[Token]):
+    stack = []
+
+    for token in tokens:
+
+        if token.type == 'NUMBER':
+            stack.append(token)
+
+        elif token.type == 'PLUS':
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(Token('NUMBER',float(a.value) + float(b.value),0))
+
+        elif token.type == 'MINUS':
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(Token('NUMBER',float(b.value) - float(a.value),0))
+
+        elif token.type == 'DIV':
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(Token('NUMBER',float(b.value) / float(a.value),0))
+
+        elif token.type == 'MUL':
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(Token('NUMBER',float(a.value) * float(b.value),0))
+
+
+    return stack[0].value
 
 def print_tokens(tokens: array[Token]):
     for i in tokens:
