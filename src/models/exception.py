f3a1b4196943d6bef36540849150d5c58252e9ce
VladislavIlin7 <svladilin@gmail.com>
2025-10-15 15:19:45 +0300

    Исправил недочёты

--- src/models/exception.py
+++ src/models/exception.py
@@ -2,4 +2,3 @@ class ModExeption(Exception): pass
 class FloorDivExeption(Exception): pass
 class ManyArgExeption(Exception): pass
 class ManyOpExeption(Exception): pass
-class ModExeption(Exception): pass
